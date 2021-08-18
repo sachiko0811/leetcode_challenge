423b532c525df5d424b10b2dae5b8c11b42b0e05
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-08-18 16:39:14 -0700

    filter method

--- leetcode0818.js
+++ leetcode0818.js
@@ -1,1 +1,8 @@
-console.log("Hello world")
+console.log("Hello world");
+
+// .filter() method
+let arr1 = [1,2,3,4,5,6];
+let res1 = arr1.filter(ele => {
+  return ele % 2 === 0;
+});
+console.log(res1); // [2, 4, 6]
