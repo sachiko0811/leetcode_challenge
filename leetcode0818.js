df0fb0eee856beb783aff87a15f33b4554a5f0e3
sachiko0811 <sachikonovel@yahoo.co.jp>
2021-08-18 16:46:34 -0700

    includes()

--- leetcode0818.js
+++ leetcode0818.js
@@ -1,8 +1,16 @@
 console.log("Hello world");
 
-// .filter() method
+/* .filter() */
 let arr1 = [1,2,3,4,5,6];
 let res1 = arr1.filter(ele => {
   return ele % 2 === 0;
 });
 console.log(res1); // [2, 4, 6]
+
+/* .includes() */
+let arr2 = [1,2,3,4,5,6,7,8,9,10];
+// let res2 = arr2.includes(1); // true
+// let res2 = arr2.includes(2, 1); // true
+let res2 = arr2.includes(4, 4); // false
+console.log(res2); // true
+
